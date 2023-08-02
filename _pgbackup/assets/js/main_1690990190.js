32351397cc5ff1e281ca210a73a74ab158407ddc
oea12 <66128082+oea12@users.noreply.github.com>
2023-08-02 09:40:13 -0600

    New op

--- _pgbackup/assets/js/main_1690990190.js
+++ _pgbackup/assets/js/main_1690990190.js
@@ -70,24 +70,6 @@ License: https://themeforest.net/licenses/standard
 		});
 
 	}
-    function initMap() {
-  const myLatLng = {
-    lat: 40.12150192260742,
-    lng: -100.45039367675781
-  };
-  const map = new google.maps.Map(document.getElementById("gmp-map"), {
-    zoom: 4,
-    center: myLatLng,
-    fullscreenControl: false,
-    zoomControl: true,
-    streetViewControl: false
-  });
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "My location"
-  });
-}
 
 	// [3. Backgrounds]
 	function ed_backgrounds() {
